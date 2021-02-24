a216aa932626c2e820ddf1a3d6740231e7c82cec
Vania <vanialadev@gmail.com>
2021-02-24 11:49:26 -0300

    profile

--- react/moveit-next/src/components/Profile.tsx
+++ react/moveit-next/src/components/Profile.tsx
@@ -1,10 +1,15 @@
-export function Profile(){
-  return(
-    <div>
-      <img src="https://github.com/vanialadev.png" alt="Vania Almeida" />
+import styles from "../styles/components/Profile.module.css"
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/vanialadev.png" alt="Vania Almeida"/>
       <div>
         <strong> Vania Almeida</strong>
-        <p>Level 1</p>
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level 1
+        </p>
       </div>
     </div>
   )
