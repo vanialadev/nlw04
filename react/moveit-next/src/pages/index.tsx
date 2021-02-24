e99eebfcdb6610951c693b206b6ca10575753326
Vania <vanialadev@gmail.com>
2021-02-24 13:52:41 -0300

    favicon e titulo pagina

--- react/moveit-next/src/pages/index.tsx
+++ react/moveit-next/src/pages/index.tsx
@@ -3,10 +3,15 @@ import {Profile} from "../components/Profile";
 import styles from "../styles/pages/Home.module.css"
 import {CompletedChallenges} from "../components/CompletedChallenges";
 import {Countdown} from "../components/Countdown";
+import Head from "next/head";
 
 export default function Home() {
   return (
   <div className={styles.container}>
+    <Head>
+      <title>Início | move.it</title>
+    </Head>
+
     <ExperienceBar />
 
     <section>
