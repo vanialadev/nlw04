e623f1bb2d03419c4fa5ccf389c21716de604fda
Vania <vanialadev@gmail.com>
2021-02-24 11:26:00 -0300

    profile

---
+++ react/moveit-next/src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile(){
+  return(
+    <div>
+      <img src="https://github.com/vanialadev.png" alt="Vania Almeida" />
+      <div>
+        <strong> Vania Almeida</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  )
+}
